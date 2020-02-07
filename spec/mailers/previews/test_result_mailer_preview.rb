2516644d366a4ef8dc0f2c6360a6e0584dba1903
Meliq Pilosyan <melopilosyan@gmail.com>
2020-02-08 01:47:39 +0400

    Move mailers previews under spec

--- spec/mailers/previews/test_result_mailer_preview.rb
+++ spec/mailers/previews/test_result_mailer_preview.rb
@@ -1,4 +1,4 @@
-# Preview all emails at http://localhost:3000/rails/mailers/test_result_mailer/
+# Preview all emails at http://localhost:3000/rails/mailers/test_result_mailer
 
 class TestResultMailerPreview < ActionMailer::Preview
   def notify_not_passed
